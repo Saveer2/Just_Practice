b930916c42d841cc07e2cd624b4e23157ed27e03
SaveerMore <98229535+Saveer2@users.noreply.github.com>
2025-03-23 22:42:10 +0530

    Add files via upload

---
+++ private class inheritance.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class Parent{
+    public:
+        int x=10;
+        Parent(){
+            cout<<"Parent Constructor Called!!"<<endl;
+        }
+
+        void display(){
+            cout<<"Parent Display : x="<<x<<endl;
+        }
+    protected:
+        int y=20;
+    private:
+        int z=30;
+};
+class child : private Parent{
+    public:
+        void show(){
+            Parent::display();
+        }
+
+        child(){
+            cout<<"Child Constructor Called!!"<<endl;
+        }
+
+        void display(){
+            cout<<"Child Display : x="<<x<<" | y="<<y<<endl;
+        }
+};
+
+int main(){
+    child obj;
+    obj.show();
+    obj.display();
+    return 0;
+}
